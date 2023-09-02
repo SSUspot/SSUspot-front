3d3805e785e552a83e7e147314773130ef06e947
mingi1123 <min_gi1123@naver.com>
2023-09-02 18:21:14 +0900

    feat: first setting

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>SSUpot</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <p> index.tsx </p>
+
+    </>
+  );
+};
+
+export default Home;
+
