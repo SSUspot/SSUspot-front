ff60196c02e0fb3ded70f25ede8e761332a025ad
mingi1123 <min_gi1123@naver.com>
2023-11-01 23:05:14 +0900

    feat: render spots to map

--- pages/main/map.tsx
+++ pages/main/map.tsx
@@ -1,11 +1,33 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Header from "../../component/layout/header";
-import Filter from "../../component/layout/filter";
-import Navigation from "../../component/layout/navigation";
-import KakaoMap from "../../component/KakaoMap";
+import type { NextPage } from 'next';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Head from 'next/head';
+import Header from '../../component/layout/header';
+import Filter from '../../component/layout/filter';
+import Navigation from '../../component/layout/navigation';
+import KakaoMap from '../../component/KakaoMap';
 
-const Home: NextPage = () => {
+const MapMain: NextPage = () => {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    if (spots.length === 0) {
+      handleGetSopts();
+    }
+    console.log(spots);
+  });
+
+  const handleGetSopts = async () => {
+    try {
+      const apiUrl = 'http://localhost:8080/api/spots';
+      const response = await axios.get(apiUrl);
+      console.log(response);
+      setSpots(response.data);
+    } catch (error) {
+      console.error('에러:', error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -17,9 +39,9 @@ const Home: NextPage = () => {
       <Filter />
       <Navigation />
 
-      <KakaoMap />
+      <KakaoMap spots={spots} />
     </>
   );
 };
 
-export default Home;
+export default MapMain;
