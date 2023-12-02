7d0864cf5f39a8ab98d4fded44146851abb5dd01
mingi1123 <min_gi1123@naver.com>
2023-12-02 17:36:11 +0900

    feat: make spots , add spotTag, spotInfo

---
+++ pages/main/[spotId].tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import axios from 'axios';
+import Header from '../../component/layout/header';
+import Filter from '../../component/layout/filter';
+import Navigation from '../../component/layout/navigation';
+
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import Image from 'next/image';
+
+import spots from '../../component/spot/spots';
+
+const SpotPage = () => {
+  const router = useRouter();
+  const { spotId } = router.query;
+
+  return (
+    <>
+      <Head>
+        <title>SSUspot</title>
+        <link rel='icon' href='/favicon.png' />
+      </Head>
+
+      <Header />
+      <Filter />
+      <Navigation />
+
+      <Container>
+        <SpotFrame>
+          <SpotName> {spots[Number(spotId)-1].spotName} </SpotName>
+          <SpotInfo>{spots[Number(spotId)-1].spotInfo}</SpotInfo>
+          <SpotImage src={spots[Number(spotId)-1].spotImage} alt={spots[Number(spotId)-1].spotName} />
+        </SpotFrame>
+      </Container>
+    </>
+  );
+};
+
+export default SpotPage;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SpotFrame = styled.div`
+  width: 90%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5vh;
+  gap: 2vh;
+`;
+
+const SpotName = styled.div`
+  font-family: 'Giants';
+  font-size: 6vh;
+  margin-bottom: 2vh;
+`;
+
+const SpotImage = styled(Image)`
+  width: 60%;
+  height: 50%;
+  object-fit: cover;
+  object-position: center center;
+  top: 0;
+  left: 0;
+`;
+
+const SpotInfo = styled.div`
+  width: 60%;
+  display: flex;
+  font-family: 'GiantsLight';
+  font-size: 2vh;
+  line-height: 200%;
+`;
---
+++ type/spot.ts
@@ -0,0 +1,14 @@
+
+interface Spot {
+    id: number;
+    latitude: number;
+    longitude: number;
+    spotLevel: number;
+    spotName: string;
+    spotImage: string;
+    spotTag: string[];
+    spotInfo: string
+  }
+  
+  export default Spot;
+  
