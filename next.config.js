af7cb49e9418826ff044edfc1b10330a55f4f98a
mingi1123 <min_gi1123@naver.com>
2023-12-09 16:11:20 +0900

    feat(root): set root hero page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   images: {
-    domains: ['ibb.co', 'i.ibb.co', 'velog.velcdn.com', 'ssuspot.s3.ap-northeast-2.amazonaws.com'],
+    domains: [
+      'ibb.co',
+      'i.ibb.co',
+      'velog.velcdn.com',
+      'ssuspot.s3.ap-northeast-2.amazonaws.com',
+      'upload.wikimedia.org',
+    ],
   },
   reactStrictMode: false,
   swcMinify: true,
