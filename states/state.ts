36a717723d76d8e62e41373eb0ee783006f79db2
Sihyun Lee <leesh3597@gmail.com>
2023-12-09 17:37:54 +0900

    refactor: remove user state

--- states/state.ts
+++ states/state.ts
@@ -1,14 +1,5 @@
 import { atom } from 'recoil';
 
-export interface UserInfo {
-  id: number | null;
-  email: string | null;
-  userName: string | null;
-  nickname: string | null;
-  profileMessage: string | null;
-  profileImageLink: string | null;
-}
-
 interface AccessTokenInfo {
   accessToken: string | null;
   accessTokenExpiredIn: number | null;
@@ -23,13 +14,5 @@ export const accessTokenState = atom<AccessTokenInfo>({
     accessTokenExpiredIn: null,
     refreshToken: null,
     refreshTokenExpiredIn: null,
-  },
-});
-
-export const userState = atom<UserInfo | null | any>({
-  key: 'userState',
-  default: () => {
-    const storedUser = localStorage.getItem('user');
-    return storedUser ? JSON.parse(storedUser) : null;
   },
 });
