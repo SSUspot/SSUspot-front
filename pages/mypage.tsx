8e56a9bc2c7708631aa61e9916ab7fe03cafb747
Sihyun Lee <leesh3597@gmail.com>
2023-12-09 19:55:57 +0900

    Resolved conflicts with main branch

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,136 +1,58 @@
-import { useEffect, useState } from 'react';
-import { UserInfo } from '../states/state';
-import Header from '../component/layout/header';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Image from 'next/image';
 import Modal from '../component/common/modal';
+import Navigation from '../component/layout/navigation';
 import Router from 'next/router';
+import axiosInstance from '../utils/axiosInstance';
+import Header from '../component/layout/header';
+import MyPageHeader from '../component/mypage/header';
+import MyPagePosts from '../component/mypage/posts';
+
+import User from '../type/user';
+import Post from '../type/post';
 
 const MyPage: React.FC = () => {
-  const [user, setUser] = useState<UserInfo | null>(null);
-
-  // 팔로워, 팔로잉 숫자 클릭하면 각각에 맞게 리스트 보여주는 모달
-  const [showModal, setShowModal] = useState(false);
-  const [modalType, setModalType] = useState(''); // 팔로워 또는 팔로잉
-
-  // 프로필 편집 모드 (nickname, statusMessage의 초기값은 DB에서 가져오는 값으로 초기화함)
-  const [isEditing, setIsEditing] = useState(false);
-  const [nickname, setNickname] = useState('');
-  const [statusMessage, setStatusMessage] = useState('');
+  const [userInfo, setUserInfo] = useState<User>();
+  const [userPosts, setUserPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedUser = localStorage.getItem('user');
-      if (storedUser) {
-        const parsedUser = JSON.parse(storedUser);
-        setUser(parsedUser);
-        setNickname(parsedUser.nickname);
-        setStatusMessage(parsedUser.profileMessage);
-      }
-    }
+    // User;
+    axiosInstance
+      .get('/api/users')
+      .then((response) => {
+        console.log('/api/users', response.data);
+        setUserInfo(response.data);
+      })
+      .catch((error) => {
+        console.log('/api/users error', error);
+      });
+    // Post
+    axiosInstance
+      .get('/api/posts/users/me', {
+        params: {
+          page: 1,
+          size: 10,
+        },
+      })
+      .then((response) => {
+        console.log('/api/posts/users/me', response.data);
+        setUserPosts(response.data);
+      })
+      .catch((error) => {
+        console.log('/api/posts/users/me', error);
+      });
   }, []);
-
-  const clickModal = (type: string) => {
-    setShowModal(!showModal);
-    setModalType(type);
-  };
-
-  var posting: number = 4;
-  var follower: number = 3;
-  var following: number = 3;
-
-  // 포스팅 개수에 따라 배열 생성
-  const userPosts = Array.from({ length: posting }, (_, index) => ({
-    id: index + 1,
-  }));
-
-  const handleEditClick = () => {
-    // 프로필 편집 버튼을 클릭하면 프로필 편집 모드로 전환
-    setIsEditing(true);
-  };
-
-  const handleSaveClick = () => {
-    // "프로필 편집 완료" 버튼을 클릭하면 프로필 편집 모드 종료
-    setIsEditing(false);
-    // DB에 새로운 nickname과 statusMessage를 저장하는 것 구현
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('user');
-    Router.push('/login');
-  };
 
   return (
     <>
       <Header />
+      <Navigation />
       <Container>
-        <ImageBox>
-          <StyledImage
-            src="https://picsum.photos/150"
-            alt=""
-            width={150}
-            height={150}
-          ></StyledImage>
-        </ImageBox>
-        <ContentContainer>
-          <Content1>
-            {isEditing ? ( // 프로필 편집 모드인 경우 입력 필드 표시
-              <ProfileEditInput
-                type="text"
-                value={nickname}
-                onChange={(e) => {
-                  setNickname(e.target.value);
-                }}
-              />
-            ) : (
-              nickname // 편집 모드가 아닌 경우 기존 닉네임 표시
-            )}
-            {!isEditing && ( // 편집 모드가 아닌 경우에만 프로필 편집 버튼 표시
-              <Button onClick={handleEditClick}>프로필 편집</Button>
-            )}
-            <Button onClick={handleLogout}>로그아웃</Button>
-          </Content1>
-          <Content2>
-            <p>게시물 {posting}</p>
-            <p
-              onClick={() => clickModal('팔로워')}
-              style={{ cursor: 'pointer' }}
-            >
-              팔로워 {follower}
-            </p>
-            {showModal && (
-              <Modal clickModal={clickModal} modalType={modalType} />
-            )}
-            <p
-              onClick={() => clickModal('팔로잉')}
-              style={{ cursor: 'pointer' }}
-            >
-              팔로잉 {following}
-            </p>
-          </Content2>
-          <Content2 style={{ fontWeight: 400 }}>
-            {!isEditing ? (
-              // 편집 모드가 아닌 경우 기존 상태 메시지 표시
-              <p>{statusMessage}</p>
-            ) : (
-              // 편집 모드인 경우 상태 메시지 입력 필드 표시
-              <ProfileEditInput
-                type="text"
-                value={statusMessage}
-                onChange={(e) => setStatusMessage(e.target.value)}
-              />
-            )}
-            {isEditing && ( // 편집 모드일 때 "프로필 편집 완료" 버튼 표시
-              <Button onClick={handleSaveClick}>편집 완료</Button>
-            )}
-          </Content2>
-        </ContentContainer>
+        <MyPageHeader userInfo={userInfo} postLength={userPosts.length} />
+        <DivisionBar />
+        <MyPagePosts userPosts={userPosts} />
       </Container>
-      <PostingContainer>
-        {userPosts.map((post) => (
-          <Posting key={post.id}></Posting>
-        ))}
-      </PostingContainer>
     </>
   );
 };
@@ -138,117 +60,19 @@ const MyPage: React.FC = () => {
 export default MyPage;
 
 const Container = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-contents: flex-start;
-  padding: 50px 50px 60px;
-  margin-left: 50px;
-  width: calc(100vw-50px);
-  height: 150px;
-  border-bottom: solid;
-  border-color: rgba(0, 0, 0, 0.1);
-`;
-
-const ImageBox = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-contents: center;
-  width: 250px;
-  height: 300px;
-`;
-
-const StyledImage = styled.img`
-  border-radius: 75px;
-`;
-
-const ContentContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-`;
-
-const Content1 = styled.div`
-  width: auto;
-  height: 50px;
-  display: flex;
-  flex-direction: row;
   align-items: center;
-  font-weight: 600;
-  font-size: 20px;
 `;
 
-const Content2 = styled.div`
-  width: 300px;
-  height: 50px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  font-weight: 600;
-  font-size: 16px;
-`;
+const DivisionBar = styled.div`
+  width: 85%;
+  border: 0.5px solid rgba(79, 76, 76, 0.4);
+  margin-bottom: 3vh;
 
-const Button = styled.button`
-  display: inline-block;
-  width: auto;
-  padding: 10px 10px;
-  border-radius: 5px;
-  border: none;
-  background-color: rgba(0, 0, 0, 0.1);
-  font-weight: 600;
-  font-size: 14px;
-  margin-left: 30px;
-  margin-top: 5px;
-
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.3);
+  @media (max-width: 735px) {
+    margin-bottom: 2vh;
   }
-`;
-
-const PostingContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 360px 360px 360px;
-  margin: 10px 50px;
-  gap: 10px;
-`;
-
-const Posting = styled.div`
-  border: none;
-  background-color: rgba(0, 0, 0, 0.1);
-`;
-
-export const ModalBox = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.4);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const ModalContent = styled.div`
-  padding: 10px 30px;
-  width: 300px;
-  height: 400px;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  background-color: #2f2f2f;
-`;
-
-const ProfileEditInput = styled.input`
-  width: auto;
-  padding: 8px;
-  margin-top: 5px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-  font-size: 16px;
-  outline: none;
 `;
