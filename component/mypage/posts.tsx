98a63d5bec18cec014448f9c18e6b3f3a8bbbbf0
mingi1123 <min_gi1123@naver.com>
2023-12-09 16:57:08 +0900

    feat(mypage): set post cursor overlay

--- component/mypage/posts.tsx
+++ component/mypage/posts.tsx
@@ -7,14 +7,26 @@ import axiosInstance from '../../utils/axiosInstance';
 import Post from '../../type/post';
 
 const MyPagePosts: React.FC<{ userPosts: Post[] }> = ({ userPosts }) => {
+  const [hoveredPost, setHoveredPost] = useState<number | null>(null);
+
   return (
     <Container>
       {userPosts.length === 0 ? (
         <p> 게시물이 없습니다. </p>
       ) : (
         <PostGrid>
-          {userPosts.map((post) => (
-            <PostImageWrapper key={post.id}>
+          {userPosts.map((post, index) => (
+            <PostImageWrapper
+              key={post.id}
+              onMouseEnter={() => setHoveredPost(index)}
+              onMouseLeave={() => setHoveredPost(null)}
+              isHovered={hoveredPost === index}
+            >
+              {hoveredPost === index && (
+                <Overlay>
+                  <OverlayContent> 게시물 보기 </OverlayContent>
+                </Overlay>
+              )}
               <PostImage src={post.imageUrls[0]} alt={post.title} />
             </PostImageWrapper>
           ))}
@@ -28,7 +40,7 @@ export default MyPagePosts;
 
 const Container = styled.div`
   width: 100%;
-  font-family: 'Arial', sans-serif;
+  font-family: 'GmarketSansMedium';
   margin: 0;
   padding: 0;
 `;
@@ -42,20 +54,11 @@ const PostGrid = styled.div`
 `;
 
 const PostImageWrapper = styled.div`
+  position: relative;
   width: 100%;
   aspect-ratio: 1;
   overflow: hidden;
   cursor: pointer;
-
-  &:hover {
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.3);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
 `;
 
 const PostImage = styled.img`
@@ -63,4 +66,20 @@ const PostImage = styled.img`
   height: 100%;
   object-fit: cover;
   object-position: center;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const OverlayContent = styled.div`
+  color: white;
 `;
